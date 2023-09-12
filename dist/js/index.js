544178578ff45f1c069ea60d766cd55e96363fd5
Vinicius Henrique <112169674+vinicius123henrique321@users.noreply.github.com>
2023-09-12 10:43:35 -0300

    update: html

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let x = 10;
+x = 12;
+console.log(x);
+let y = 12;
+let z = [];
+let a = 12;
+let b = 13;
+z.push(a);
+z.push(b);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let x: Number = 10;
+
+x = 12;
+
+console.log(x)
+
+let y = 12
+
+let z: number[] = []
+
+let a = 12
+let b = 13
+
+z.push(a)
+z.push(b)
